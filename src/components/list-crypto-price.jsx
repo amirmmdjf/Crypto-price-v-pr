2fee0dd491dbe70116da0bbf4a82a29e44b91306
Amirmohammad <amirmmdjf2006@gmail.com>
2024-03-29 22:18:31 +0330

    Update

--- src/components/list-crypto-price.jsx
+++ src/components/list-crypto-price.jsx
@@ -34,86 +34,88 @@ const Test = () => {
 
 
     return (
-    <>
-        <h2 className='text-center'>Crypto price</h2>
-        <div className='w-[900px] m-auto my-14 max-[600px]:w-[450px] max-[400px]:w-[350px]' >
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 350 }} aria-label="sticky table">
-                    <TableHead className='bg-gray-100' style={{
-                        marginBottom: '100px',
-                        position: 'sticky',
-                        top: '0',
-                        zIndex: '999',
-                    }}>
-                        <TableRow >
-                            <TableCell className='name-col' style={{ fontSize: '17px', fontWeight: '600' }}>Name</TableCell>
-                            <TableCell className='price-col' style={{ fontSize: '17px', fontWeight: '600' }} align="center">Price</TableCell>
-                            <TableCell className='hideOnSmallScreen' style={{ fontSize: '17px', fontWeight: '600' }} align="center">1h %</TableCell>
-                            <TableCell className='oneday-col' style={{ fontSize: '17px', fontWeight: '600' }} align="center">24h %</TableCell>
-                            <TableCell className='hideOnSmallScreen' style={{ fontSize: '17px', fontWeight: '600' }} align="center">Market Cap</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {/* Render table rows based on 'data' state */}
-                        {data.slice(0, 150).map((crypto) => (
-                            <TableRow key={crypto.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-
-                                <TableCell
-                                    className='name-col'
-                                    style={{
-                                        fontWeight: '600'
-                                    }}
-                                    component="th" scope="row">
-                                    {crypto.name}
-                                </TableCell>
-
-                                <TableCell
-                                    className='price-col'
-                                    style={{ fontWeight: '600' }}
-                                    align="center">{crypto.price_usd} $
-                                </TableCell>
-
-                                <TableCell className='hideOnSmallScreen' align="center">
-                                    <div style={{
-                                        width: '80px',
-                                        backgroundColor: crypto.percent_change_1h[0] === '-' ? '#ea3943' : '#16c784',
-                                        padding: '8px 0px ',
-                                        display: "flex",
-                                        justifyContent: 'center',
-                                        borderRadius: '5px',
-                                        color: "white"
-                                    }}>
-                                        {crypto.percent_change_1h} %
-                                    </div>
-                                </TableCell>
-
-                                <TableCell
-                                    className='oneday-col'
-                                    align="center">
-                                    <div style={{
-                                        width: '80px',
-                                        backgroundColor: crypto.percent_change_24h[0] === '-' ? '#ea3943' : '#16c784',
-                                        padding: '8px 0px ',
-                                        display: "flex",
-                                        justifyContent: 'center',
-                                        borderRadius: '5px',
-                                        color: "white"
-                                    }}>
-                                        {crypto.percent_change_24h} %
-                                    </div>
-                                </TableCell>
-
-                                <TableCell className='hideOnSmallScreen' align="center" style={{
-                                    fontWeight: "600"
-                                }}
-                                >{crypto.market_cap_usd} <h3 style={{ color: '#4d4d4d', fontWeight: "500" }}>{crypto.symbol}</h3></TableCell>
+        <>
+            <h2 className='text-center mt-8 text-[28px] font-bold text-white'>Crypto price</h2>
+            <div className='w-[900px] m-auto my-14 max-[600px]:w-[450px] max-[400px]:w-[350px]' >
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 350 }} aria-label="sticky table">
+                        <TableHead className='bg-gray-100' style={{
+                            marginBottom: '100px',
+                            position: 'sticky',
+                            top: '0',
+                            zIndex: '999',
+                        }}>
+                            <TableRow >
+                                <TableCell className='name-col' style={{ fontSize: '17px', fontWeight: '600' }}>Name</TableCell>
+                                <TableCell className='price-col' style={{ fontSize: '17px', fontWeight: '600' }} align="center">Price</TableCell>
+                                <TableCell className='hideOnSmallScreen' style={{ fontSize: '17px', fontWeight: '600' }} align="center">1h %</TableCell>
+                                <TableCell className='oneday-col' style={{ fontSize: '17px', fontWeight: '600' }} align="center">24h %</TableCell>
+                                <TableCell className='hideOnSmallScreen' style={{ fontSize: '17px', fontWeight: '600' }} align="center">Market Cap</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    </>
+                        </TableHead>
+                        <TableBody>
+                            {/* Render table rows based on 'data' state */}
+                            {data.slice(0, 150).map((crypto, index) => (
+                                <TableRow key={crypto.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+
+                                    <TableCell
+                                        className='name-col'
+                                        style={{
+                                            fontWeight: '600',
+                                        }}
+                                        component="th" scope="row">
+                                        <div className='flex'>
+                                            <h3 className='mr-3'>{index + 1}</h3>  <h3>{crypto.name}</h3>
+                                        </div>
+                                    </TableCell>
+
+                                    <TableCell
+                                        className='price-col'
+                                        style={{ fontWeight: '600' }}
+                                        align="center">{crypto.price_usd} $
+                                    </TableCell>
+
+                                    <TableCell className='hideOnSmallScreen' align="center">
+                                        <div style={{
+                                            width: '80px',
+                                            backgroundColor: crypto.percent_change_1h[0] === '-' ? '#ea3943' : '#16c784',
+                                            padding: '8px 0px ',
+                                            display: "flex",
+                                            justifyContent: 'center',
+                                            borderRadius: '5px',
+                                            color: "white"
+                                        }}>
+                                            {crypto.percent_change_1h} %
+                                        </div>
+                                    </TableCell>
+
+                                    <TableCell
+                                        className='oneday-col'
+                                        align="center">
+                                        <div style={{
+                                            width: '80px',
+                                            backgroundColor: crypto.percent_change_24h[0] === '-' ? '#ea3943' : '#16c784',
+                                            padding: '8px 0px ',
+                                            display: "flex",
+                                            justifyContent: 'center',
+                                            borderRadius: '5px',
+                                            color: "white"
+                                        }}>
+                                            {crypto.percent_change_24h} %
+                                        </div>
+                                    </TableCell>
+
+                                    <TableCell className='hideOnSmallScreen' align="center" style={{
+                                        fontWeight: "600"
+                                    }}
+                                    >{crypto.market_cap_usd} <h3 style={{ color: '#4d4d4d', fontWeight: "500" }}>{crypto.symbol}</h3></TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </>
     );
 }
 
